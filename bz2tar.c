9144ecfd6483e0bff0e7a8f62d3b6f7b31d52423
Hugo Neves de Carvalho <hugonvsc@gmail.com>
2019-12-04 03:38:20 -0300

    Updates

--- bz2tar.c
+++ bz2tar.c
@@ -1,99 +1,138 @@
-#define _XOPEN_SOURCE 500
+// Defines
 #define FILENAME_MAX 1000
+#define _XOPEN_SOURCE 500
+
+//Standart
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
+#include <errno.h>
+
+// Using on mkdir
+#include <sys/stat.h>
+#include <sys/types.h>
+
+// Files
 #include <dirent.h>
-#include <bzlib.h>
 #include <ftw.h>
 
+// structs
 static int one (const struct dirent *unused){
     return 1;
 }
 
-void cp_files(char *filename){
-    FILE *fptr_orig, *fptr_dest; 
-    char c;  
-  
-    // Open one file for reading 
-    fptr_orig = fopen(filename, "r"); 
-    if (fptr_orig == NULL){ 
-        printf("Cannot open file %s \n", filename); 
-        exit(0); 
-    } 
-    
-    // Open another file for writing
-    char dest_tmp[FILENAME_MAX] = "./t/";
-    strcat(dest_tmp, filename);
 
-    fptr_dest = fopen(dest_tmp, "w"); 
-    if (fptr_dest == NULL){ 
-        printf("Cannot open file %s \n", filename); 
-        exit(0); 
-    } 
+// Global scope
+char folder_name[FILENAME_MAX];
+
+
+// Funcs
+int create_dest_folder(char *dest_name){
+    memcpy(folder_name, dest_name, strlen(dest_name) - 3); 
+    folder_name[strlen(dest_name) - 4] = '\0';
+
+    int check = mkdir(folder_name, 0777); 
   
-    // Read contents from file 
-    c = fgetc(fptr_orig); 
-    while (c != EOF){ 
-        fputc(c, fptr_dest); 
-        c = fgetc(fptr_orig); 
+    // check if directory is created or not 
+    if (check){
+        printf("Unable to create directory\n"); 
+        exit(1); 
     } 
-  
-    fclose(fptr_orig); 
-    fclose(fptr_dest); 
+
+    return 1;
+
 }
 
-int nftwfunc(const char *fpath, const struct stat *stat, int tflags, struct FTW *ftwbuf){
+
+int nftw_copy_func(const char *in_path, const struct stat *stat, int tflags, struct FTW *ftwbuf){
     if(tflags == FTW_D){
-        printf("| %s\n", fpath);
+        printf("%s\n", in_path);
+        char out_path[FILENAME_MAX];
+        strcpy(out_path, folder_name);
+        strcat(out_path, "/");
+        strcat(out_path, in_path);
+        mkdir(out_path, 0777); 
+
     }
 
     if(tflags == FTW_F){
-        printf("path %s\n", fpath);
-        const char token = '/';
-        char *ret;
-        ret = strrchr(fpath, token);
-        ret = ret + 1;
+        printf("%s\n", in_path);
+        char out_path[FILENAME_MAX];
+        strcpy(out_path, folder_name);
+        strcat(out_path, "/");
+        strcat(out_path, in_path);
+        
+        char buff[BUFSIZ];
+        FILE    *in, *out;
+        size_t  n;
 
-        printf("ret %s\n", ret);
-        cp_files(ret);
+        in  = fopen(in_path, "rb");
+        out = fopen(out_path, "wb");
+        while ( (n=fread(buff,1,BUFSIZ,in)) != 0 ) {
+            fwrite( buff, 1, n, out );
+        }
 
-        // char cmd[FILENAME_MAX] = "bzip2 ";
-        // strcat(cmd, ret);
-        // FILE *fp = popen(cmd, "w");
-        // pclose(fp);
-        // printf("PASSED\n");
+        fclose(in);
+        fclose(out);
+
     }
-
-
     return 0;
 }
 
-
-int list_dirs (char path[]){
+void copy_files(char path[]){
     int fd_limit = 5;
-    int flags = FTW_CHDIR;
+    int flags = FTW_CHDIR & FTW_DEPTH;
     int ret;
 
-    ret = nftw(path, nftwfunc, fd_limit, flags);
+    ret = nftw(path, nftw_copy_func, fd_limit, flags);
 }
 
 
-void compress_file(char filename[], char orig_filename[]){
+int nftw_zip_func(const char *in_path, const struct stat *stat, int tflags, struct FTW *ftwbuf){
+    if(tflags == FTW_F){
+        char cmd[FILENAME_MAX] = "bzip2 ";
+        strcat(cmd, in_path);
+        FILE *fp = popen(cmd, "w");
+        pclose(fp);
+    }
+    return 0;
+}
+
+void bzip2_files(char path[]){
+    int fd_limit = 5;
+    int flags = FTW_CHDIR & FTW_DEPTH;
+    int ret;
+
+    ret = nftw(path, nftw_zip_func, fd_limit, flags);
+}
+
+
+
+int nftw_rm_func(const char *in_path, const struct stat *stat, int tflags, struct FTW *ftwbuf){
+    int rv = remove(in_path);
+    if (rv)
+        perror(in_path);
+
+    return rv;
+}
+
+void rmrf_files(char path[]){
+    int fd_limit = 5;
+    int flags = FTW_DEPTH;
+    int ret;
+
+    ret = nftw(path, nftw_rm_func, fd_limit, flags);
+}
+
+void compress_file(char filename[]){
     char cmd[FILENAME_MAX] = "tar cf ";
     strcat(cmd, filename);
     strcat(cmd, " ");
-    strcat(cmd, orig_filename);
-    // strcat(cmd, ".bz2");
+    strcat(cmd, folder_name);
 
-    printf("%s\n", cmd);
     FILE *fp = popen(cmd, "w");
     pclose(fp);
 }
-
-
-
 
 
 int main(int argc, char *argv[]){
@@ -104,12 +143,22 @@ int main(int argc, char *argv[]){
         orig_dir = argv[1];
         dest_name = argv[2];
     }
-    
+
+    // Create folder .bz2
+    create_dest_folder(dest_name);
+
+    // Copy files to .bz2 folder
+    copy_files(orig_dir);
+
+    // Run bzip2 on files
+    bzip2_files(folder_name);
+
 
     // Read orig dir recursively
-    list_dirs(orig_dir);
-    // compress_file(dest_name, orig_dir);
-    // do_stuff();
+    compress_file(dest_name);
+
+    // Delete .bz2 folder
+    rmrf_files(folder_name);
 
 
     return 0;
